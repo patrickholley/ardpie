5e9912b678e4118fb577e14d25c88d93070394a8
Tricky <patrick@holley.dev>
2024-11-26 20:49:13 -0600

    removed unused struct

--- src/db/users.rs
+++ src/db/users.rs
@@ -224,8 +224,3 @@ fn get_expires_at() -> usize {
     let since_the_epoch = start.duration_since(UNIX_EPOCH).expect("Time went backwards");
     (since_the_epoch + Duration::from_secs(60 * 60)).as_secs() as usize
 }
-
-#[derive(Debug)]
-struct LoginError;
-
-impl warp::reject::Reject for LoginError {}
